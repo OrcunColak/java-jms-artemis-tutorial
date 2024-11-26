3b992a4ce2fab00836e2765cc942eb4984548215
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-26 11:54:57 +0300

    upgrade to jakarta

--- src/main/java/com/colak/consumer/JMSConsumer.java
+++ src/main/java/com/colak/consumer/JMSConsumer.java
@@ -1,13 +1,12 @@
 package com.colak.consumer;
 
+import jakarta.jms.Connection;
+import jakarta.jms.Message;
+import jakarta.jms.MessageConsumer;
+import jakarta.jms.Queue;
+import jakarta.jms.Session;
+import jakarta.jms.TextMessage;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
-
-import javax.jms.Connection;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
 
 class JMSConsumer {
 
--- src/main/java/com/colak/producer/JMSProducer.java
+++ src/main/java/com/colak/producer/JMSProducer.java
@@ -1,14 +1,14 @@
 package com.colak.producer;
 
 
+import jakarta.jms.Connection;
+import jakarta.jms.DeliveryMode;
+import jakarta.jms.MessageProducer;
+import jakarta.jms.Queue;
+import jakarta.jms.Session;
+import jakarta.jms.TextMessage;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
-import javax.jms.Connection;
-import javax.jms.DeliveryMode;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
 
 class JMSProducer {
 
